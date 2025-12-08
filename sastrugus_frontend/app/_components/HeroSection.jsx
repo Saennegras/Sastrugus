c8e254ca8faba1a18989f39f9b4255efe31e026e
Saennegras <saennegras@proton.me>
2025-12-08 13:47:12 +0000

    create hero section - set up flexible code for further varieties of HS, create fall backs for feedbacks

---
+++ sastrugus_frontend/app/_components/HeroSection.jsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+const HeroSection = ({ imgSrc,  headline, theme = "turquoise" }) => {
+    return (
+        <section className="hero">
+            <div className="hero__background">
+                <img src={imgSrc || "/assets/hero.png"} alt="" />
+            </div>
+            <div className={'hero__headline hero__headline--${theme}'}>
+                {headline || <h3>Headline missing</h3>}
+            </div>
+            <button className={'btn btn--medium btn--${theme}'}>
+                <Link href="/login">Sign Up</Link>
+            </button>
+            <img 
+                className={'hero__logo hero__logo--${theme}'} 
+                src="/assets/Star1.svg" 
+            />
+        </section>
+    )
+};
+
+export default HeroSection
--- sastrugus_frontend/app/page.js
+++ sastrugus_frontend/app/page.js
@@ -1,8 +1,16 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import HeroSection from './_components/HeroSection';
 
 export default function Home() {
+  const heroHeadline = (
+    <>
+      <h1>Join the Sastrugus</h1>
+      <h1>Workshop Community</h1>
+    </>
+  )
   return (
     <main>
+      <HeroSection headline={heroHeadline}/>
     </main>
   )
 }
