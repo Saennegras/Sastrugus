9954735548385a376a1f9f7cd9f0a576bcc3e5dc
Daniel Kovacs <info@kovix.info>
2025-12-11 23:40:11 +0100

    init payment api endpoint

---
+++ sastrugus_backend/src/api/routes/payment.ts
@@ -0,0 +1,13 @@
+export default {
+  routes: [
+    // {
+    //  method: 'GET',
+    //  path: '/payment',
+    //  handler: 'payment.exampleAction',
+    //  config: {
+    //    policies: [],
+    //    middlewares: [],
+    //  },
+    // },
+  ],
+};
